a399bfd6328afc82149fa09fd32c48149f45a094
TheSciCoder3000 <drjjdevilla2002@gmail.com>
2024-09-23 20:37:17 +0800

    adding navigation animations

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,25 +1,28 @@
 import Navbar from "../components/Navbar"
-import { Route, Routes } from "react-router-dom"
+import { Route, Routes, useLocation } from "react-router-dom"
 import Home from "./Home"
 import About from "./About"
 import Services from "./Services"
 import Login from "./Login"
 import Register from "./Register"
 import Contact from "./Contact"
+import { AnimatePresence } from "framer-motion"
 
 function App() {
-
+  const location = useLocation();
   return (
     <>
       <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/services" element={<Services />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-      </Routes>
+      <AnimatePresence mode="wait">
+        <Routes location={location} key={location.pathname}>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </AnimatePresence>
     </>
   )
 }
